fcb923fe300bee7280a55b1672ee0d36ed14da72
Reis567 <matheusdosreis9@gmail.com>
2023-08-31 17:43:21 -0300

    error catch and app.use

--- modules/express.js
+++ modules/express.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const UserModel = require('../src/models/user.models')
 const app = express();
+app.use(express.json());
 
 app.get('/users',(req,res)=>{
     const users = [
@@ -14,9 +15,13 @@ app.get('/users',(req,res)=>{
     res.status(200).json(users)
 });
 
-app.post('/users',(req,res)=>{
-    const user = UserModel.create(req.body)
-    res.status(201).json(user);
+app.post('/users',async (req,res)=>{
+    try{
+        const user = await UserModel.create(req.body)
+        res.status(201).json(user);
+    }catch(error){
+        res.status(500).send(error.message);
+    }
 })
 
 const port = 8080
