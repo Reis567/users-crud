f90e3e94f3b9ec6144704dd2b6a2c6d858f610c9
Reis567 <matheusdosreis9@gmail.com>
2023-08-31 18:03:33 -0300

    get by id

--- modules/express.js
+++ modules/express.js
@@ -3,27 +3,37 @@ const UserModel = require('../src/models/user.models')
 const app = express();
 app.use(express.json());
 
-app.get('/users',(req,res)=>{
-    const users = [
-        {
-            firstName:'John',
-            lastName:'Cena',
-            email:'johncenawwe@email.com',
-            password:'iamjohn123',
-        },
-    ];
-    res.status(200).json(users)
+app.get('/users', async (req, res) => {
+    try {
+        const users = await UserModel.find({});
+
+        res.status(200).json(users)
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
 });
 
-app.post('/users',async (req,res)=>{
-    try{
+app.get('/users/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const users = await UserModel.findById(id);
+
+        return res.status(200).json(users)
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+});
+
+app.post('/users', async (req, res) => {
+    try {
         const user = await UserModel.create(req.body)
         res.status(201).json(user);
-    }catch(error){
+    } catch (error) {
         res.status(500).send(error.message);
     }
 })
 
 const port = 8080
 
-app.listen(port, ()=> console.log(`Rodando na porta ${port} !`));
+app.listen(port, () => console.log(`Rodando na porta ${port} !`));
