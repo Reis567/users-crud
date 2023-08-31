ba36769daa4699826fc8cc2dced105e43bc2c3d3
Reis567 <matheusdosreis9@gmail.com>
2023-08-31 18:18:08 -0300

    error message

--- modules/express.js
+++ modules/express.js
@@ -41,9 +41,11 @@ app.patch('/users/:id', async (req, res) => {
 
         res.status(200).json(user);
     } catch (error) {
-
+        res.status(500).send(error.message);
     }
-})
+});
+
+
 
 const port = 8080
 
